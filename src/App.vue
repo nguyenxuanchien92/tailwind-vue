7732109fcb7ce4ddffd4c4cee6ef87d8ca88c4bb
nguyenxuanchien92 <nguyenxuanchien92@gmail.com>
2023-08-08 17:39:13 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,17 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <card-layout/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import CardLayout from './components/CardLayout.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld, CardLayout
   }
 }
 </script>
@@ -21,8 +22,16 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
+
+img {
+  width: 100px;
+  height: 100px;
+}
+
 </style>
---
+++ src/components/CardLayout.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="card">
+    <div class="header">
+      <div class="header-content"></div>
+    </div>
+    <div class="content">
+      <p>How to make this material card ?</p>
+    </div>
+    <div class="footer"></div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+<style scoped>
+.card {
+  width: 200px;
+  height: 300px;
+  background-color: aqua;
+}
+
+.content {
+  margin: 0 auto;
+  width: 10rem;
+}
+
+.header-content {
+  width: 10rem;
+  height: 2rem;
+  background-color: brown;
+  margin: 1.5rem auto 1.5rem auto;
+  border-radius: 0.5rem;
+}
+
+.content > b {
+  letter-spacing: 0;
+  line-height: 1.375;
+  font-weight: 600;
+  font-size: 1.25rem;
+}
+</style>
